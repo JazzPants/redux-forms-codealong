d15113ebf084bb13fd29a82eaf8aa28849a8382e
Leighton <leighton.l14@gmail.com>
2022-03-18 09:55:08 +1100

    Finished!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
-import CreateTodo from './components/todos/CreateTodo';
+import React, { Component } from "react";
+import CreateTodo from "./components/todos/CreateTodo";
+import { connect } from "react-redux";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CreateTodo />
+        <ul>
+          {this.props.todos.map((e) => (
+            <li>{e}</li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+    todos: state.todos,
+  };
+};
+
+export default connect(mapStateToProps)(App);
